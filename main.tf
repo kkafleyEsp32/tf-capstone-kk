dc052f8eeb1fa9829338e5fa91603e0e3bdc2de1
Kishor Kafley <kafley@hotmail.com>
2026-01-21 09:49:16 +0000

    test

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ module "Vms_app1" {
   instance_type = "t3.micro"
   subnets = ["subnet-0a413d769fe98ba0b", "subnet-043f1f8151b681cca", "subnet-04f47a3252bdf06e6"]
   #Security_group = module.Security_group.op_sg_id
+  web_sg = [module.Web_SG01.web_sg_id]
   num_of_vm = 1
   env = "dev"
 }
