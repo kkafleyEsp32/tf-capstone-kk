8f45e9cfcbde28aa9b90bfc7ad1c6e3adeb7e093
Kishor Kafley <kafley@hotmail.com>
2026-01-21 07:35:39 +0000

    new repo

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "boa-terraform-state-im-jan25-kishor"
-    key    = "dev/terraform.tfstate"
+    key    = "dev-cs/terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
     dynamodb_table = "terraform-locks-kishor"
