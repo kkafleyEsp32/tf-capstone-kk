342079bfa8088f325284432518109d44fcfd2744
Kishor Kafley <kafley@hotmail.com>
2026-01-21 09:20:07 +0000

    updates

--- main.tf
+++ main.tf
@@ -3,8 +3,6 @@ provider "aws" {
 }
  
 module "Vms_app1" {
-  #source = "./module/ec2-instance"
-  #source = "git::https://github.com/kkafleyEsp32/kk-tf-aws-module-1.git//ec2-instance?ref=main"
   source = "git::https://github.com/kkafleyEsp32/kk-repo-1.git//ec2-instance?ref=main"
   region-1 = "ap-south-1"
   instance_type = "t3.micro"
@@ -14,7 +12,16 @@ module "Vms_app1" {
   env = "dev"
 }
 
+module "Web_SG01" {
+  source = "git::https://github.com/Vishwanathms/tf-aws-modules-jan26.git//security-groups?ref=main"
+  project = "vishwa"
+  vpc_id = data.aws_vpc.default.id
+}
  
+data "aws_vpc" "default" {
+  default = true
+}
+
 output "vm_private_ips01" {
   value = module.Vms_app1.vm_priv_pips
 }
