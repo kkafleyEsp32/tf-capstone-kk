679ce896ca233d42dd896bf19f8ec543878892d2
Kishor Kafley <kafley@hotmail.com>
2026-01-21 07:33:27 +0000

    new repo

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "boa-terraform-state-im-jan25-kishor"
+    key    = "dev/terraform.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+    dynamodb_table = "terraform-locks-kishor"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+ 
+module "Vms_app1" {
+  #source = "./module/ec2-instance"
+  #source = "git::https://github.com/kkafleyEsp32/kk-tf-aws-module-1.git//ec2-instance?ref=main"
+  source = "git::https://github.com/kkafleyEsp32/kk-repo-1.git//ec2-instance?ref=main"
+  region-1 = "ap-south-1"
+  instance_type = "t3.micro"
+  subnets = ["subnet-0a413d769fe98ba0b", "subnet-043f1f8151b681cca", "subnet-04f47a3252bdf06e6"]
+  #Security_group = module.Security_group.op_sg_id
+  num_of_vm = 1
+  env = "dev"
+}
+
+ 
+output "vm_private_ips01" {
+  value = module.Vms_app1.vm_priv_pips
+}
